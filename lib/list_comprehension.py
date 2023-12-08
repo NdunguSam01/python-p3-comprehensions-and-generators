1d54d2ead32e0c18604cf0050c8dbda60ac7ec5f
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-12-08 12:09:26 +0300

    Correct output but the tests are failing

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # evens_list=[num for num in num_list if (num % 2 == 0)]
+    if [num for num in num_list if (num % 2 == 0)] == 0:
+        print([])
+    else:
+        print([num for num in num_list if (num % 2 == 0)])
 
 def make_exclamation(sentence_list):
-    pass
+    if sentence_list == '':
+        print(f"{[sentence_list]}")
+    else:
+        print([f"{sentence}!" for sentence in sentence_list])
+
+return_evens([0, 1, 3, 5, 7, 8, 9])
+return_evens(range(1, 10, 2))
+return_evens(range(10))
+sentence_list = [
+            "I like computers",
+            "I require coffee",
+            "Live long and prosper",
+        ]
+make_exclamation(sentence_list)
